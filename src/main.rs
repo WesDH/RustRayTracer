cc59d158f58de8d0c56ac0f8dfbc7c81572c1ecd
wes <wesleydh85@gmail.com>
2025-01-18 00:01:53 -0600

    Add Cargo.* and main.rs

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+pub mod tuples;
+pub mod tuples_test;
+// Operations on vectors and points:
+//fn add_tuple(a1: Struct, a2: Point) -> Point {}
+
+#[allow(dead_code)]
+fn main() {
+    println!("Hello, world!");
+    // let p1 = point(0.1,0.2, 0.3);
+    // let v1 = vector(0.1,0.2, 0.3);
+}
+
